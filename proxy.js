221cf65266b9a0a6537d771b3ff80427edab0803
calimanco <calimanco@qq.com>
2021-05-21 17:05:17 +0800

    process error

--- proxy.js
+++ proxy.js
@@ -1,26 +1,33 @@
 const http = require('http')
 const httpProxy = require('http-proxy')
 
-//
-// Create a proxy server with custom application logic
-//
 const proxy = httpProxy.createProxyServer({})
 
-//
-// Create your custom server and just call `proxy.web()` to proxy
-// a web request to the target passed in the options
-// also you can use `proxy.ws()` to proxy a websockets request
-//
 const server = http.createServer(function (req, res) {
   // You can define here your custom logic to handle the request
   // and then proxy the request.
-  console.log('Proxy evoke', req.headers.host)
-  if (req.headers.host.match(/^demo\.com$/i)) {
-    proxy.web(req, res, { target: 'http://127.0.0.1:3000' })
+  const { headers, method, url } = req
+  console.log('Proxy evoke', headers.host, method, url)
+  // Proxy to demo.com
+  if (headers.host.match(/^demo\.com$/i)) {
+    proxy.web(req, res, { target: 'http://127.0.0.1:3000' }, error => {
+      console.error(error.message)
+      res.statusCode = 504
+      res.end()
+    })
+    return
   }
-  if (req.headers.host.match(/^api\.demo\.com$/i)) {
-    proxy.web(req, res, { target: 'http://127.0.0.1:4000' })
+  // Proxy to api.demo.com
+  if (headers.host.match(/^api\.demo\.com$/i)) {
+    proxy.web(req, res, { target: 'http://127.0.0.1:4000' }, error => {
+      console.error(error.message)
+      res.statusCode = 504
+      res.end()
+    })
+    return
   }
+  res.statusCode = 404
+  res.end()
 })
 
 console.log('Proxy start')
